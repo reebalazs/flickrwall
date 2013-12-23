e1855d3320d658f0a54c9466227e53e56d31521d
Balazs Ree <ree@greenfinity.hu>
2013-12-23 12:52:13 +0100

    Configuration file

--- flickrwall-install.py
+++ flickrwall-install.py
@@ -4,26 +4,21 @@ from __future__ import print_function
 import sys
 import os
 import plistlib
+import ConfigParser
+
+from flickrwall import (
+    CONFIG_FILE_NAME,
+    CONFIG_SECTION,
+    get_config,
+)
 
 
 def here_path():
     return os.path.abspath(os.path.dirname(sys.modules[__name__].__file__))
 
-def gen_plist(api_key=None):
+def gen_plist(o):
     here = here_path()
     plist_path = os.path.expanduser('~/Library/LaunchAgents/local.flickrwall.plist')
-    if api_key is None:
-        try:
-            plist = plistlib.readPlist(plist_path)
-        except Exception:
-            pass
-        else:
-            try:
-                api_key = plist['ProgramArguments'][2]
-            except (IndexError, KeyError):
-                pass
-    if not api_key:
-        api_key = input('Please enter your Flickr API key: ')
     plist = {}
     plist['RunAtLoad'] = True
     plist['Label'] = 'local.flickrwall'
@@ -33,15 +28,32 @@ def gen_plist(api_key=None):
     plist['ProgramArguments'] = [
         os.path.join(here, 'bin', 'python'),
         os.path.join(here, 'flickrwall.py'),
-        api_key,
     ]
     plistlib.writePlist(plist, plist_path)
 
 
+def gen_config(**kw):
+    o = get_config()
+    o.update(**kw)
+    if not 'api_key' in o:
+        o['api_key'] = raw_input('Please enter your Flickr API key: ')
+    home = os.path.expanduser("~")
+    path = os.path.join(home, CONFIG_FILE_NAME)
+    c = ConfigParser.ConfigParser()
+    c.add_section(CONFIG_SECTION)
+    for key, value in o.iteritems():
+        c.set(CONFIG_SECTION, key, value)
+    c.write(open(path, 'w'))
+    return o
+
+
 def main():
     args = sys.argv[1:]
-    api_key = args[0] if len(args) >= 1 else None
-    gen_plist(api_key=api_key)
+    kw = {}
+    if len(args) >= 1:
+        kw['api_key'] = args[0]
+    o = gen_config(**kw)
+    gen_plist(o)
 
 
 if __name__ == '__main__':
--- flickrwall.py
+++ flickrwall.py
@@ -3,23 +3,43 @@ from __future__ import print_function
 
 import flickrapi
 import os
-import sys
 import time
 import datetime
 import re
 import urllib
 import socket
 import shutil
+import ConfigParser
 
 # timeout in seconds
 timeout = 20
 socket.setdefaulttimeout(timeout)
 
-def feed(api_key):
+CONFIG_FILE_NAME = '.flickrwall'
+CONFIG_SECTION = 'flickrwall'
+
+def get_config():
+    home = os.path.expanduser("~")
+    path = os.path.join(home, CONFIG_FILE_NAME)
+    o = dict(
+        base_dir=os.path.join(home, 'Downloads', 'iWallpaper'),
+        download_nr=20,
+        flush_days=30,
+    )
+    if os.path.isfile(path):
+        c = ConfigParser.ConfigParser()
+        c.read(path)
+        o.update(dict(c.items(CONFIG_SECTION)))
+    o['download_nr'] = int(o['download_nr'])
+    o['flush_days'] = int(o['flush_days'])
+    return o
+
+
+def feed(o):
     """Infinite feed of interesting images.
     Only landscape images above a given size are considered.
     """
-    flickr = flickrapi.FlickrAPI(api_key)
+    flickr = flickrapi.FlickrAPI(o['api_key'])
     page = 1
     while True:
         sets = flickr.interestingness_getList(page=page, extras="url_o,original_format")
@@ -33,16 +53,16 @@ def feed(api_key):
         page += 1
 
 
-def download(base_dir, api_key, nr=20):
+def download(o):
     """Download a number of new images
     """
     re_filename = re.compile(r'\W+')
     downloaded = 0
-    for i, photo in enumerate(feed(api_key)):
+    for i, photo in enumerate(feed(o)):
         kw = photo.attrib
         safe_title = re_filename.sub('_', kw['title'])
         file_name = safe_title + '_%(id)s.%(originalformat)s' % kw
-        path = os.path.abspath(os.path.join(base_dir, file_name))
+        path = os.path.abspath(os.path.join(o['base_dir'], file_name))
         if not os.path.exists(path):
             try:
                 tmp_path, _headers = urllib.urlretrieve(kw['url_o'])
@@ -57,29 +77,28 @@ def download(base_dir, api_key, nr=20):
         else:
             print("HIT (%s)" % (file_name, ))
         #print photo.attrib['url_o']
-        if downloaded >= nr:
+        if downloaded >= o['download_nr']:
             break
 
     # http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
 
 
-def flush(base_dir, days=30):
+def flush(o):
     now = time.time()
-    for file_name in os.listdir(base_dir):
-        fullpath = os.path.join(base_dir, file_name)
-        if os.stat(fullpath).st_mtime < (now - days * 86400):
+    for file_name in os.listdir(o['base_dir']):
+        fullpath = os.path.join(o['base_dir'], file_name)
+        if os.stat(fullpath).st_mtime < (now - o['flush_days'] * 86400):
             if os.path.isfile(fullpath):
                 print("DEL (%s)" % (file_name, ))
                 os.remove(fullpath)
 
 
 def main():
-    (api_key, ) = sys.argv[1:]
-    base_dir = "/Users/ree/Downloads/iWallpaper"
     ts_begin = datetime.datetime.now()
     print('BEGIN RUN:', ts_begin);
-    flush(base_dir, days=30)
-    download(base_dir, api_key=api_key, nr=20)
+    o = get_config()
+    flush(o)
+    download(o)
     ts_end = datetime.datetime.now()
     print('END RUN, elapsed time:', ts_end - ts_begin);
 
